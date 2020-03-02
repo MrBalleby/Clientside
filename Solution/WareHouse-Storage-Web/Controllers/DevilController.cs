933f3f00b9846a700335216d109cb47b5eefb175
Mark Balleby Hansen <mbal@verdo.com>
2020-03-02 13:15:16 +0100

    Added Project ASP.NET CORE -removed all data -added new controller -added html documents -added ccs documents

---
+++ Solution/WareHouse-Storage-Web/Controllers/DevilController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace WareHouse_Storage_Web.Controllers
+{
+    public class DevilController : Controller
+    {
+        public IActionResult Devil()
+        {
+            return View();
+        }
+
+    }
+}
